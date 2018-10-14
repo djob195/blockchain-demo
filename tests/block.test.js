4619875a871316b1bf589fbbdd779ab479011e42
djob195 <djorellanab@outlook.com>
2018-10-14 17:05:23 -0600

    Proof work

--- tests/block.test.js
+++ tests/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('../objects/block');
+const {DIFFICULTY} = require('../config/index');
 
 describe('Block', () =>{
     let data, lastBlock, block;
@@ -15,5 +16,18 @@ describe('Block', () =>{
 
     it('Set the `lastHash` to match of the last block', () =>{
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('Generate a hash  that matches the difficulty', () =>{
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+        console.log(block.toString());
+    });
+
+    it('lower the difficulty for slowly mined blocks', () =>{
+        expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+    });
+
+    it('raise the difficulty for quickly mined blocks', () =>{
+        expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
     });
 });
--- tests/dev/dev-test-blockchain.js
+++ tests/dev/dev-test-blockchain.js
@@ -1,10 +1,10 @@
 const Blockchain = require('../../objects/blockchain');
 
 console.log("Start Blockchain: ");
-let blockchain = new Blockchain();
-console.log(`Blockchain Length ${blockchain.chain.length}`);
-let newBlock = blockchain.addBlock('data');
-console.log("New block:");
-console.log(newBlock.toString());
-console.log(`Blockchain Length ${blockchain.chain.length}`);
+const blockchain = new Blockchain();
+
+for(let i= 0; i < 10; i++)
+{
+    console.log(blockchain.addBlock(`foo ${i}`).toString());
+}
 
