18deda94c000bdee697c594e25ba09ddbc8156d9
djob195 <djorellanab@outlook.com>
2018-10-13 23:11:47 -0600

    Handler message

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -13,10 +13,32 @@ class P2pServer{
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('Connection', socket => this.connectSocket(socket));
         console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+        this.connectToPeers();
     }
 
     connectSocket(socket){
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+            socket.on('open', () => this.connectSocket(socket));
+        });
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+
+        });
     }
 }
+
+module.exports = P2pServer;
